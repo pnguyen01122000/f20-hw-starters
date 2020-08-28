1a3a7b55fac28538bc5dd979b6f63b5afa22abc4
pnguyen01122000 <70299477+pnguyen01122000@users.noreply.github.com>
2020-08-27 18:32:39 -0700

    Update h00.cpp

--- h00/h00.cpp
+++ h00/h00.cpp
@@ -1,13 +1,13 @@
 /**
- *  @author Put your name here
- *  @date Put the date here (Semester is OK)
+ *  @author Put your name here Phuc Nguyen
+ *  @date Put the date here (Semester is OK) 08/27/2020
  *  @file h00.cpp
  */
 #include <iostream>
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO ARE YOU?";  // Add your Canvas login name
+string STUDENT = "Phuc Nguyen";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 /**
@@ -16,10 +16,8 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    // Add your implementation comments here
-
-    // Write your code here
-
+    cout << STUDENT << " starter code." << endl;
+    cout << "[1, 1]" << endl;
     return 0;
 }
 
